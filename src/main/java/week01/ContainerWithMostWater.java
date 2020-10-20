3b8eacf72fbc0518da9731f0348a887b583c682c
Jingshuai-cloud <xujingshuai@gmail.com>
2020-10-20 21:06:46 +1100

    ContainerWithMostWater

--- src/main/java/week01/ContainerWithMostWater.java
+++ src/main/java/week01/ContainerWithMostWater.java
@@ -8,10 +8,10 @@ public class ContainerWithMostWater {
     //two pointer
     public static int MaxArea(int[] height){
         int left = 0; int right = height.length - 1;
-        int maxArea = 0;
-        while(left < right){
-            maxArea = Math.max(maxArea, Math.min(height[left], height[right]) * (right-left));
-            if(height[left] < height[right]) left++;
+        int maxArea =  0;
+        while (left < right) {
+            maxArea = Math.max(maxArea, Math.min(height[left], height[right]) * (right - left));
+            if (height[left] < height[right]) left++;
             else right--;
         }
         return maxArea;
